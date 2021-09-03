63a57b71964ff7b15f5f1c95e358dd3de15b4aaf
xmoonanx <xmoonanx@gmail.com>
2021-09-03 16:18:57 +0900

    const, let, var

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert("hi");
+const a= 5; //const : constant -> cant update Variable Data.
+
+let myName = "WJ"; // let : can update Variable Data.
+
+var b = 5; //var : variable : past Variable type, can Update Data, but not protect Data. So recommand use const, let.
+
+console.log( a + 1);
+console.log(b+2);
+console.log("My name is " + myName);
+
+myName = "WooJin";
+console.log("My name is " + myName);
+b = 2;
+console.log(b+2);
+
