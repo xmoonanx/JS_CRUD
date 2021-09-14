28f2ad4a925f45ab6765c466826f0a4d4b31b89c
xmoonanx <xmoonanx@gmail.com>
2021-09-14 16:01:54 +0900

    Object and Funtion Recap

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const toBuy = ["potato", "tomato", "pizza"];
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  div: function (a, b) {
+    console.log(a / b);
+  },
+  multi: function (a, b) {
+    console.log(a * b);
+  },
+  powerof: function (a, b) {
+    console.log(a ** b);
+  },
+};
 
-console.log(toBuy);
-toBuy[2] = "water";
-console.log(toBuy);
-toBuy.push("one");
-console.log(toBuy);
+calculator.add(6, 5);
+calculator.minus(7, 4);
+calculator.div(9, 3);
+calculator.multi(5, 2);
+calculator.powerof(2, 4);
