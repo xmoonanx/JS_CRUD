7e05ec16fdb57d7e39be25717d9efb35cad4d9f8
xmoonanx <xmoonanx@gmail.com>
2021-09-14 16:30:59 +0900

    Return Recap

--- app.js
+++ app.js
@@ -21,3 +21,9 @@ const minusResult = calculator.minus(plusResult, 10);
 const multiResult = calculator.multi(10, minusResult);
 const divResult = calculator.div(multiResult, plusResult);
 const powerResult = calculator.powerof(divResult, minusResult);
+
+// In function, write the other works after return,
+// the other works not execute!
+// If there R the other works before return,
+// this works is execute!
+// In other words, it is executed until return.
